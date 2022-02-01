13e917dac7d52defadc99661b98ae00f602debce
JustinW1233 <79864211+JustinW1233@users.noreply.github.com>
2022-02-01 14:53:52 -0700

    added on the listOf5Letter words

--- wordBank.py
+++ wordBank.py
@@ -1,5 +1,15 @@
-easyList = {"Hard", "Ship", "Game", "Week", "June", "fart", "poop", "Wall", "Hole", "Nail", 
+listOf4Letter = {"Hard", "Ship", "Game", "Week", "June", "fart", "poop", "Wall", "Hole", "Nail", 
 "Star", "Jeff", "Ring", "Tree", "Ball", "Ammo", "Four", "Fire", "Girl", "Jazz" }
-mediumList = {}
-hardList = {"Bottle", "Letter", "Bridge", "Outlet", "Puzzle", "Hijack", "Backup", "Jungle", 
+print(listOf4Letter)
+
+listOf5Letter = {"Apple","Alone","Beach","Beams","Clock","China","Cream", "Dream","Drive", 
+"Earth","Enemy","Error","Final","Force","Glass","Guide","Heart","Horse","Image","Index","Japan",
+"Judge","Knife","Level","Light","Major","Metal","Music","Night","North","Offer","Order","Peace","Price",
+"Peach","Queen","Radio","River","Scene","Shock","Space","Sugar","Theme","Trust","Union","Unity","Value",
+"Video","Waste","Watch","Youth","Zebras"}
+print(listOf5Letter)
+
+
+listO6Letter = {"Bottle", "Letter", "Bridge", "Outlet", "Puzzle", "Hijack", "Backup", "Jungle", 
 "Injury", "Jewish", "Mother", "Squirm", "Wizard", "Zodiac", "Jetlag", "Quarry", "Webcam", "Blowup", "Liquor", "Wacked"  }
+print(listO6Letter)
