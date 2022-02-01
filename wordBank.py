a980826030d0d0d5e5bf11ec6960a3c9c5293100
JustinW1233 <79864211+JustinW1233@users.noreply.github.com>
2022-02-01 15:19:24 -0700

    Created function that will return a list based off the difficulty chosen

--- wordBank.py
+++ wordBank.py
@@ -1,15 +1,26 @@
+import random
+
+
 listOf4Letter = {"Hard", "Ship", "Game", "Week", "June", "fart", "poop", "Wall", "Hole", "Nail", 
 "Star", "Jeff", "Ring", "Tree", "Ball", "Ammo", "Four", "Fire", "Girl", "Jazz" }
-print(listOf4Letter)
+#print(listOf4Letter)
 
 listOf5Letter = {"Apple","Alone","Beach","Beams","Clock","China","Cream", "Dream","Drive", 
 "Earth","Enemy","Error","Final","Force","Glass","Guide","Heart","Horse","Image","Index","Japan",
 "Judge","Knife","Level","Light","Major","Metal","Music","Night","North","Offer","Order","Peace","Price",
 "Peach","Queen","Radio","River","Scene","Shock","Space","Sugar","Theme","Trust","Union","Unity","Value",
 "Video","Waste","Watch","Youth","Zebras"}
-print(listOf5Letter)
+#print(listOf5Letter)
 
 
 listO6Letter = {"Bottle", "Letter", "Bridge", "Outlet", "Puzzle", "Hijack", "Backup", "Jungle", 
 "Injury", "Jewish", "Mother", "Squirm", "Wizard", "Zodiac", "Jetlag", "Quarry", "Webcam", "Blowup", "Liquor", "Wacked"  }
-print(listO6Letter)
+#print(listO6Letter)
+
+def getListOffDifficulty(diffi):
+    if(diffi == "easy"):
+        return listOf4Letter
+    if(diffi == "medium"):
+        return listOf5Letter
+    if(diffi == "hard"):
+        return listO6Letter
