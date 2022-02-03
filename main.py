5d57f05461b4ee52424260dbd5156b0b89cbe37a
ziggityofficial <60523976+ziggityofficial@users.noreply.github.com>
2022-02-03 15:20:27 -0700

    added letter checking

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 from graphics import *
 from letter import *
+
+correct_guess = False
+
+blank_space = Blank_Space("h", 0)
+blank_space = Blank_Space("e", 1)
+blank_space = Blank_Space("l", 2)
+blank_space = Blank_Space("l", 3)
+blank_space = Blank_Space("o", 4)
+
+for b in blank_spaces:
+    print(str(b))
+    was_guess_correct = b.was_letter_guessed("l")
+    if was_guess_correct == True:
+        correct_guess = True
+
+if correct_guess == False:
+    lives -= 1
+    print("incorrect")
+
+print(correct_guess)
