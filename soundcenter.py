b92bfee487e33f3547d186f898e0aa64d7f667cc
ziggityofficial <60523976+ziggityofficial@users.noreply.github.com>
2022-03-08 15:26:18 -0700

    sound function done, sound folder started

--- soundcenter.py
+++ soundcenter.py
@@ -1,4 +1,15 @@
-from winsound import *
+from pygame import mixer
+import time
 
-def playfilesound(filename):
-    return PlaySound(str(filename), SND_FILENAME)
+
+#pip install pygame
+def play_sound_file(value):
+
+    mixer.init()
+    mixer.music.load('sounds/' + value + '.mp3')
+    mixer.music.play()
+    while mixer.music.get_busy():  # wait for music to finish playing
+        time.sleep(1)
+
+    
+play_sound_file("main")
